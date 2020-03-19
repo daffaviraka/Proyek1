fe2256340da066158cb75ca581c98546642d368c
daffaviraka <daffanew99@gmail.com>
2020-03-19 19:09:11 +0700

    Menambahkan Laporan

--- src/frontend/Laporan.java
+++ src/frontend/Laporan.java
@@ -5,17 +5,38 @@
  */
 package frontend;
 
+import backend.Barang;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author owner
  */
 public class Laporan extends javax.swing.JFrame {
-
+    Barang b = new Barang();
     /**
      * Creates new form Laporan
      */
+    
+    public void tampilkanData(){
+        String[] kolom = {"Id Barang", "Nama Barang", "Stok", "Harga"};
+        ArrayList<Barang> list = new Barang().getAll();
+        Object rowData[] = new Object[4];
+        
+        jTableBarang.setModel(new DefaultTableModel(new Object[][] {}, kolom));
+        for (Barang br : list) {
+            rowData[0] = br.getId_barang();
+            rowData[1] = br.getNama_barang();
+            rowData[2] = br.getStok();
+            rowData[3] = br.getHarga();
+            
+            ((DefaultTableModel)jTableBarang.getModel()).addRow(rowData);
+        }
+    }
     public Laporan() {
         initComponents();
+        tampilkanData();
     }
 
     /**
@@ -27,17 +48,48 @@ public class Laporan extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTableBarang = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("LAPORAN BARANG MASUK");
+
+        jTableBarang.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTableBarang);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(135, 135, 135)
+                .addComponent(jLabel1)
+                .addContainerGap(137, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 246, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -79,5 +131,8 @@ public class Laporan extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTableBarang;
     // End of variables declaration//GEN-END:variables
 }
